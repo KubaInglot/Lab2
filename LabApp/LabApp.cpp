fc3fb931d166cd560b7c95ac7bd5234a7e46dd70
DESKTOP-6A0RELE\Piotrek <pawelec.piotrek@gmail.com>
2020-12-27 19:45:38 +0100

    resharper refactor

--- LabApp/LabApp.cpp
+++ LabApp/LabApp.cpp
@@ -1,71 +1,35 @@
-﻿
-//Uppgiftinstruktioner
-/*
-//Du ska skapa ett fristående program som utför uppgifterna nedan.
-//Börja med att lösa uppgift ett och färdigställ den uppgiften innan du börjar på uppgift 2.
-//Ditt program ska läsa filerna som omnämns ifrån ”current working directory”.
-
-//Samtliga filer är i textformat med ”\n” som radslut.Du kan om du önskar
-//använda pipes och redirects för att lösa filinläsningen och utskriften istället
-//för std::ifstream och std::ofstream.Kommentera i sådant fall det i koden
-//tillsammans med exempel på hur programmet används.
-
-
-//1. Filen names.txt innehåller namn och personnummer på följande form.Filen
-//innehåller flera personer.
-//Förnamn Efternamn
-//YYMMDDNNNN
-//Adressrad
-
-	//Din uppgift är att omvandla indatat till formen :
-//Efternamn, Förnamn
-//Adressrad
-//Filen names.txt finner du på :
-//http://w3.miun.se/dt028g/attach/164/names.txt.
-
-
-//2. Utdatat ska indikera om personen är en man eller kvinna.Exempelvis för
-//en man kan det stå :
-//Efternamn, Förnamn[M]
-//Adressrad
-*/
-
-
-
-#include <cstdlib>
+﻿#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <string>
 #include <vector>
-
 #include "names.txt"
 #include "LabApp.h"
+
 using namespace std;
 
-
-
-
 vector<string> vec_Of_Names;
-vector <int> vec_Of_Year;
+vector<int> vec_Of_Year;
 vector<string> vec_Of_Adres;
 
 void Load_file()
 {
 	fstream file;
-	file.open("names.txt", ios::in);// ios:in = odczyt pliku 
+	file.open("names.txt", ios::in); // ios:in = odczyt pliku 
 	string line;
 	int line_number = 1;
 
 	if (file.good() == false)
 	{
 		cout << "file cant open";
-
 	}
 
 	//1
-	int line_number = 1, year_Temp;
-	string line, name_Temp, adres_Temp, control;
+	int year_Temp;
+	string name_Temp;
+	string adres_Temp;
+	string control;
 
 	while (getline(file, line))
 	{
@@ -82,13 +46,12 @@ void Load_file()
 				{
 					control = control + line[i];
 				}
-
 			}
 			year_Temp = atoi(control.c_str());
 			vec_Of_Year.push_back(year_Temp);
 			break;
 
-		case 3:adres_Temp = line;
+		case 3: adres_Temp = line;
 			vec_Of_Adres.push_back(adres_Temp);
 			break;
 
@@ -98,7 +61,6 @@ void Load_file()
 		}
 
 		line_number++;
-
 	}
 
 	file.close();
@@ -106,7 +68,6 @@ void Load_file()
 
 void Save_file(vector<string> names, vector<string> adress)
 {
-
 	fstream file;
 	file.open("names.txt", ios::out); //ios:out =zapis do pliku 
 
@@ -117,7 +78,6 @@ void Save_file(vector<string> names, vector<string> adress)
 	if (file.good() == false)
 	{
 		cout << "file cant open";
-
 	}
 
 	ByteNamn(names);
@@ -146,18 +106,17 @@ void Save_file(vector<string> names, vector<string> adress)
 	switch (line_number)
 	{
 	case 1:
-		file << name_Temp << " " << endl; break;
+		file << name_Temp << " " << endl;
+		break;
 
 	case 2:
-		file << adres_Temp << endl;  break;
-
-
+		file << adres_Temp << endl;
+		break;
 
 
 	default:
 		break;
 	}
-
 
 
 	file.close();
@@ -176,16 +135,14 @@ void ByteNamn(vector<string> names)
 			while (isalpha(names[i][nameletter]) == true)
 			{
 				temp = temp + ((names[i])[nameletter]);
-
 			}
 			name_first.push_back(temp);
 			temp = "";
-			nameletter++;// Kommer orsaka problem om det finns mer än 1 space mellan namn och effternamn
+			nameletter++; // Kommer orsaka problem om det finns mer än 1 space mellan namn och effternamn
 
 			while (isalpha(names[i][nameletter]) == true)
 			{
 				temp = temp + ((names[i])[nameletter]);
-
 			}
 			name_second.push_back(temp);
 			temp = "";
@@ -201,7 +158,6 @@ void ByteNamn(vector<string> names)
 		{
 			temp = temp + name_first[i2];
 		}
-
 	}
 }
 
@@ -211,11 +167,11 @@ bool Man_eller_kvinna(int Y) //2,4,6=man 3,5,7=kvinna
 	string control;
 	if (atoi(control.c_str) % 2 == 0)
 	{
-		return true;//male
+		return true; //male
 	}
 	else
 	{
-		return false;//kobieta
+		return false; //kobieta
 	}
 
 	//for (int i = 0; i <Y.size(); i++)
@@ -234,12 +190,6 @@ bool Man_eller_kvinna(int Y) //2,4,6=man 3,5,7=kvinna
 int main()
 {
 	Load_file();
-
-
 	ByteNamn(vec_Of_Names);
-
 	Save_file(vec_Of_Names, vec_Of_Adres);
-
-
 }
-
